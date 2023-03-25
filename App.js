05ae3e52aa88de286e7faa386453c427dce46e01
Martin Lorenzo <126398675+MartinLorenzFajardo@users.noreply.github.com>
2023-03-25 19:51:54 +0800

    added the react app (resume)
    files uploaded due to failure in pushing

---
+++ App.js
@@ -0,0 +1,133 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <div className="uwu">
+            <img src="https://i.ibb.co/jb3JJQc/1.jpg" alt="image" border="3px"/>
+            <h1>MARTIN LORENZ S. FAJARDO</h1>
+            <a href="martinlorenz.fajardo@gmail.com">martinlorenz.fajardo@gmail.com</a>
+            <p>09758764330</p>
+            <p>ACS bldg, Barlin St, Sta. Cruz, Naga City</p>
+
+<hr color="black" size= "3px;"/>
+
+            <p>Highly organized and proactive with a team-oriented mentality.</p>
+            <p>Specializes in Front-End Developing.</p>
+
+            <p className="titles"><b>PERSONAL DATA</b></p>
+<hr className="line" color="black" size= "3px;"/>
+            <p>
+                <ul>Date of Birth: September 30, 2004</ul>
+                <ul>Age: 18</ul>
+                <ul>Gender: Male</ul>
+                <ul>Status: Single</ul>
+                <ul>Citizenship: Filipino</ul>
+            </p>
+
+
+            <p className="titles"><b>COMPUTER SKILLS</b></p>
+<hr className="line" color="black" size= "3px;"/>
+            <p>
+                <ul>
+                    <li>Graphics Design</li>
+                   <li>Photo Editing</li>
+                   <li>HTML Coding</li>
+                   <li>Word Processing</li>
+                   <li>Online Research</li>
+                </ul>
+            </p>
+
+
+            <p className="titles"><b>PROGRAMMING</b></p>
+<hr className="line" color="black" size= "3px;"/>
+            <p>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>C++</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+
+                </ul>    
+            </p>
+
+
+            <p className="titles"><b>SAMPLE WORKS</b></p>
+<hr className="line" color="black" size= "3px;"/>
+            <p>
+                <a href="https://docs.google.com/presentation/u/0/d/1XA1QO0-h6uVRRQiCLHLne4hcMdjeB7K-mcC-qHE2Rlk/edit" target="_blank" rel="noopener noreferrer">PORTFOLIO</a>
+            </p>
+
+
+            <p className="titles"><b>EDUCATION</b></p>
+<hr className="line" color="black" size= "3px;"/>
+                <p>
+                <ul>Primary:2009 - 2017</ul>
+                <ul>Naga Central School II</ul>
+
+                <ul>Secondary: 2017 - 2021</ul>
+                <ul>University of Nueva Caceres</ul>
+
+                <ul>Upper-Secondary:2021 – Present</ul>
+                <ul>University of Nueva Caceres</ul>
+                </p>
+
+
+            <p className="titles"><b>CERTIFICATION</b></p>
+<hr className="line" color="black" size= "3px;"/>
+                <ul>February 8, 2023: JavaScript Essentials</ul>
+                <ul>Cisco Networking Academy</ul>
+
+
+            <p className="titles"><b>SEMINARS / TRAINING ATTENDED</b></p>
+<hr className="line" color="black" size= "3px;"/>
+                <ul>February 14, 2023:</ul>
+                <ul>"Why Blockchain Jobs are in-demand & how Metacrafters can help you land them"</ul>
+                <ul>Metacrafters</ul>
+
+                <ul>Present:</ul>
+                <ul>ETH PROOF: Beginner EVM Course</ul>
+    	          <ul>Metacrafters</ul>
+
+                <ul>Present:</ul>
+                <ul>SOL PROOF: Beginner Course</ul>
+	              <ul>Metacrafters</ul>
+
+
+            <p className="titles"><b>ACHIEVEMENT</b></p>
+<hr className="line" color="black" size= "3px;"/>
+                <ul>February 28, 2023:</ul>
+                <ul>Development Research</ul>
+	              <ul>Title: “Project Anatomy”</ul>
+	              <ul>Language: Construct 3</ul>
+	              <ul>Role: Researcher, Artist, Programmer, & Debugger</ul>
+
+
+            <p className="titles"><b>WORKING EXPERIENCE</b></p>
+<hr className="line" color="black" size= "3px;"/>
+                <ul>2019 – 2021:</ul>
+                <ul>Assistant Editor</ul>
+                <ul>Digitek – ACS bldg, Barlin St, Naga City</ul>
+
+
+            <p className="titles"><b>REFERENCE</b></p>
+<hr className="line" color="black" size= "3px;"/>
+                <p>
+                <p>Nobelle V. Felizardo-Zantua</p>
+                <p>09504603870</p>
+                <a href="nobelle.felizardo@unc.edu.ph">nobelle.felizardo@unc.edu.ph</a>
+                </p>
+
+                <p>
+                <p>Jona Christine P. De Jesus</p>
+                <p>09385177976</p>
+                <a href= "jonachristine.dejesus@unc.edu.ph">jonachristine.dejesus@unc.edu.ph</a>
+                </p>
+        </div>
+      </div>
+
+    );
+}
+
+export default App;
